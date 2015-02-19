ea0100361921d5099530b5b999075d5cb2ad4bc1
ddkangfu <ddkangfu@gmail.com>
2015-02-19 23:34:09 +0800

    added: home test code.

--- main/views.py
+++ main/views.py
@@ -8,3 +8,19 @@ from reblog.settings import redis_con
 
 class HomeView(TemplateView):
     template_name = 'main/home.html'
+
+    def get_context_data(self, **kwargs):
+        ctx = super(HomeView, self).get_context_data(**kwargs)
+
+        # post:(id) - 使用HASH存储博客的各个字段信息
+        # posts:counter - 自增计数器
+        # posts:list - ID列表，主要用于分页
+
+        post_id_list = redis_con.lrange('posts:list', 0, 10)
+
+        if post_id_list:
+            post_list = [redis_con.hget('post:%s' % post_id, 'title') for post_id in post_id_list]
+            ctx['post_list'] = post_list
+
+        return ctx
+
