207c2c4a8a661f304952484c72005a3b94cf11c7
ddkangfu <ddkangfu@gmail.com>
2015-02-26 15:03:36 +0800

    fixed: runserver error.

--- reblog/settings.py
+++ reblog/settings.py
@@ -58,6 +58,15 @@ ROOT_URLCONF = 'reblog.urls'
 
 WSGI_APPLICATION = 'reblog.wsgi.application'
 
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
