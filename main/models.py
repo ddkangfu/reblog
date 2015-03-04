e1a52fba213bd28eb22758d93bf7f575cc295a98
ddkangfu <ddkangfu@gmail.com>
2015-03-04 23:06:00 +0800

    added: class method of Post model.

--- main/models.py
+++ main/models.py
@@ -25,3 +25,48 @@ class Post(models.Model):
                                          help_text="Used to build the entry's URL.")
     last_update = models.DateTimeField('last update', default=timezone.now)
 
+    @property
+    def is_actual(self):
+        now = timezone.now()
+        if self.start_publication and now < self.start_publication:
+            return False
+
+        if self.end_publication and now >= self.end_publication:
+            return False
+        return True
+
+    @property
+    def is_visible(self):
+        return self.is_actual and self.status == PUBLISHED
+
+    def save(self, *args, **kwargs):
+        self.last_update = timezone.now()
+        super(Post, self).save(*args, **kwargs)
+
+    def __unicode__(self):
+        return '%s: %s' % (self.title, self.get_status_display())
+
+    class Meta:
+        ordering = ['-creation_date']
+
+
+class Category(models.Model):
+    title = models.CharField(u"标题", max_length=255)
+    slug = models.SlugField('slug', unique=True, max_length=255, help_text="Used to build the category's URL.")
+    description = models.TextField('description', blank=True)
+    parent = TreeForeignKey('self', related_name='children', null=True, blank=True, verbose_name='parent category')
+    objects = TreeManager()
+
+
+class TagsEntry(models.Model):
+    tags = TagField(_('tags'))
+
+    @property
+    def tags_list(self):
+        """
+        Return iterable list of tags.
+        """
+        return parse_tag_input(self.tags)
+
+    class Meta:
+        abstract = True
